b6e3496aa161a5191d5c2603c014ffd507923c30
met4000 <nathsharwor@hotmail.com>
2020-03-05 23:14:31 +0800

    Update Receiver protocol - WIP

--- src/main/include/vision/Receiver.h
+++ src/main/include/vision/Receiver.h
@@ -9,13 +9,18 @@
 namespace wml {
   namespace vision {
     struct Receiver {
-      Receiver(std::shared_ptr<nt::NetworkTable> table = nt::NetworkTableInstance::GetDefault().GetTable("VisionTracking")) : _xOffset(table, "Target_X", &xOffset), _yOffset(table, "Target_Y", &yOffset) {}
+      Receiver(std::shared_ptr<nt::NetworkTable> table = nt::NetworkTableInstance::GetDefault().GetTable("VisionTracking")) : _ex(table, "offsetX", &ex), _ey(table, "offsetY", &ey), _RES_HEIGHT(table, "RES_HEIGHT", &RES_HEIGHT), _RES_WIDTH(table, "RES_WIDTH", &RES_WIDTH) {};
 
-      double xOffset;
-      double yOffset;
+      double ex, ey;
+      double RES_HEIGHT, RES_WIDTH;
+
+      virtual double GetHorizontalDegrees() { // returns -30 left, 30 right
+        return 60 * ex / RES_HEIGHT; // Camera FOV is about 60 deg; should probs have this in config. Also division is a good enough approx for +-30 deg.
+      };
 
      private:
-      wml::NTUtil::Slave<double> _xOffset, _yOffset;
+      wml::NTUtil::Slave<double> _ex, _ey;
+      wml::NTUtil::Slave<double> _RES_HEIGHT, _RES_WIDTH;
     };
   }
 }
