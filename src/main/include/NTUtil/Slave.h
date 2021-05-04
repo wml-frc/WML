c011b286d3bf6618dd0ce201923f34ec12e11710
Connor <connorbuchel@gmail.com>
2021-05-04 19:29:27 +0800

    NTUtil Sync

--- src/main/include/NTUtil/Slave.h
+++ src/main/include/NTUtil/Slave.h
@@ -6,62 +6,36 @@
 
 #include <networktables/NetworkTable.h>
 
+#include "NTUtil/SlaveBase.h"
 
-#define __NTUTIL__SLAVE__MACRO_FACTORY__(MainT, LongT)  \
-__NTUTIL__SLAVE__CTOR_FACTORY__(MainT, LongT)           \
-__NTUTIL__SLAVE__METHOD_FACTORY__(MainT, LongT)
 
-#define __NTUTIL__SLAVE__CTOR_FACTORY__(MainT, LongT)                                                                         \
-template <std::enable_if<std::is_convertible<T, MainT>::value>* = nullptr>                                                           \
-Slave(std::shared_ptr<nt::NetworkTable> table, std::string name, MainT *value) : _table(table), _name(name), _val(value) {    \
-  _entry = table->GetEntry(name);                                                                                             \
-  _entry.ForceSet##LongT(*value);                                                                                             \
-  _listener = _entry.AddListener([=](const nt::EntryNotification &evt) {                                                      \
-    *_val = evt.value->Get##LongT();                                                                                          \
-  }, NT_NOTIFY_UPDATE);                                                                                                       \
-}
-
-#define __NTUTIL__SLAVE__METHOD_FACTORY__(MainT, LongT)                       \
-std::enable_if_t<std::is_convertible<T, MainT>::value>                        \
-Override(MainT newVal) { _entry.ForceSet##LongT(newVal); *_val = newVal; }
+#define __NTUTIL__SLAVE__IMPLEMENTATION_FACTORY__(MainT, LongT)                     \
+template <>                                                                         \
+class Slave<MainT> : public SlaveBase<MainT> {                                      \
+  public:                                                                           \
+  /* using SlaveBase::SlaveBase; */                                                 \
+ protected:                                                                         \ 
+  virtual void ForceSetEntryValue(MainT newVal) { _entry.ForceSet##LongT(newVal); } \
+  virtual MainT GetEntryValue() { return evt.value->Get##LongT(); }                 \
+};
 
 
 namespace wml {
   namespace NTUtil {
     template <typename T>
-    class Slave {
-     public:
-      __NTUTIL__SLAVE__MACRO_FACTORY__(bool, Boolean)
-      __NTUTIL__SLAVE__MACRO_FACTORY__(double, Double)
-      __NTUTIL__SLAVE__MACRO_FACTORY__(std::string, String)
+    class Slave : public SlaveBase<T>;
 
-      // __NTUTIL__SLAVE__MACRO_FACTORY__(std::string, Raw)
+    __NTUTIL__SLAVE__IMPLEMENTATION_FACTORY__(bool, Boolean)
+    __NTUTIL__SLAVE__IMPLEMENTATION_FACTORY__(double, Double)
+    __NTUTIL__SLAVE__IMPLEMENTATION_FACTORY__(std::string, String)
 
-      __NTUTIL__SLAVE__MACRO_FACTORY__(std::vector<int>, BooleanArray)
-      __NTUTIL__SLAVE__MACRO_FACTORY__(std::vector<double>, DoubleArray)
-      __NTUTIL__SLAVE__MACRO_FACTORY__(std::vector<std::string>, StringArray)
+    // __NTUTIL__SLAVE__IMPLEMENTATION_FACTORY__(std::string, Raw)
 
-
-      template <std::enable_if<false>* = nullptr>
-      Slave(std::shared_ptr<nt::NetworkTable> table, std::string name, void* *value); // T *value
-
-      Slave(const Slave &other) : Slave(other._table, other._name, other._val) {}
-      ~Slave() { _table->RemoveEntryListener(_listener); }
-
-      std::enable_if_t<false>
-      Override(void* newVal); // T newVal
-      
-     private:
-      std::shared_ptr<nt::NetworkTable> _table;
-      nt::NetworkTableEntry _entry;
-      NT_EntryListener _listener;
-      std::string _name;
-      T *_val;
-    };
+    __NTUTIL__SLAVE__IMPLEMENTATION_FACTORY__(std::vector<int>, BooleanArray)
+    __NTUTIL__SLAVE__IMPLEMENTATION_FACTORY__(std::vector<double>, DoubleArray)
+    __NTUTIL__SLAVE__IMPLEMENTATION_FACTORY__(std::vector<std::string>, StringArray)
   } // namespace NTUtil
 }  // namespace wml
 
 
-#undef __NTUTIL__SLAVE__MACRO_FACTORY__
-#undef __NTUTIL__SLAVE__CTOR_FACTORY__
-#undef __NTUTIL__SLAVE__METHOD_FACTORY__
+#undef __NTUTIL__SLAVE__IMPLEMENTATION_FACTORY__
---
+++ src/main/include/NTUtil/SlaveBase.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <type_traits>
+#include <string>
+#include <vector>
+
+#include <networktables/NetworkTable.h>
+
+
+namespace wml {
+  namespace NTUtil {
+    template <typename T>
+    class SlaveBase {
+     public:
+      SlaveBase(std::shared_ptr<nt::NetworkTable> table, std::string name, T *value) : _table(table), _name(name), _val(value) {
+        _entry = table->GetEntry(name);
+        ForceSetEntryValue(*value);
+        _listener = _entry.AddListener([=](const nt::EntryNotification &evt) {
+          *_val = GetEntryValue();
+        }, NT_NOTIFY_UPDATE);
+      }
+
+      SlaveBase(const Slave<T> &other) : Slave(other._table, other._name, other._val) {}
+      ~SlaveBase() { _table->RemoveEntryListener(_listener); }
+
+      void Override(T newVal) {
+        ForceSetEntryValue(newVal);
+        *_val = newVal;
+      };
+
+     protected:
+      virtual void ForceSetEntryValue(T newVal) = 0; // overrides the value in `_entry` using the appropriate typed method
+      virtual T GetEntryValue() = 0; // gets the value of `_entry` using the appropriate typed method
+      
+      std::shared_ptr<nt::NetworkTable> _table;
+      nt::NetworkTableEntry _entry;
+      NT_EntryListener _listener;
+      std::string _name;
+      T *_val;
+    };
+  } // namespace NTUtil
+}  // namespace wml
