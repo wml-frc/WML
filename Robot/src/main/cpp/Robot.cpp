d1e365e001d28afcd8f6791d5913797d790878b4
Connor <connorbuchel@gmail.com>
2021-11-04 03:42:54 +0800

    Testing sim with trajectories

--- Robot/src/main/cpp/Robot.cpp
+++ Robot/src/main/cpp/Robot.cpp
@@ -7,36 +7,93 @@
 #include <iostream>
 
 #include <frc/smartdashboard/SmartDashboard.h>
+#include <frc/smartdashboard/Field2d.h>
 
-using namespace UDP_TransferNT;
-struct PID {
-	double sum = 0, prevErr = 0;
-	double kP = 0.3, kI = 0, kD = 0;
-	double pid(double dt, double input, double goal) {
-		double error = goal - input;
-		double dError = (error - prevErr)/dt;
-		sum = sum + error * dt;
+#include <frc/RobotController.h>
 
-		double output = (kP * error) + (kI * sum) + (kD * dError);
-		return output;
-	}
-}; PID pid;
+// Encoders
+#include <frc/Encoder.h>
+#include <frc/simulation/EncoderSim.h>
+
+// gyros
+#include <frc/AnalogGyro.h>
+#include <frc/simulation/AnalogGyroSim.h>
 
 
-void Robot::RobotInit() {}
-void Robot::RobotPeriodic() {}
+// Drivetrain
+#include <frc/simulation/DifferentialDrivetrainSim.h>
+#include <frc/drive/DifferentialDrive.h>
+
+#include <frc/AnalogGyro.h>
+#include <frc/Encoder.h>
+#include <frc/controller/PIDController.h>
+#include <frc/controller/SimpleMotorFeedforward.h>
+#include <frc/kinematics/DifferentialDriveKinematics.h>
+#include <frc/kinematics/DifferentialDriveOdometry.h>
+// #include <frc/motorcontrol/MotorControllerGroup.h>
+// #include <frc/motorcontrol/PWMSparkMax.h>
+#include <units/angle.h>
+#include <units/angular_velocity.h>
+#include <units/length.h>
+#include <units/velocity.h>
+// #include <wpi/numbers>
 
 
-void Robot::AutonomousInit() {}
+frc::Field2d m_field;
+
+frc::Encoder m_leftEncoder{0, 1};
+frc::Encoder m_rightEncoder{2, 3};
+
+frc::sim::EncoderSim m_leftEncoderSim{m_leftEncoder};
+frc::sim::EncoderSim m_rightEncoderSim{m_rightEncoder};
+
+frc::sim::DifferentialDrivetrainSim m_driveSim =
+	frc::sim::DifferentialDrivetrainSim::CreateKitbotSim(
+		frc::sim::DifferentialDrivetrainSim::KitbotMotor::DualCIMPerSide, // 2 CIMs per side.
+		frc::sim::DifferentialDrivetrainSim::KitbotGearing::k10p71,       // 10.71:1
+		frc::sim::DifferentialDrivetrainSim::KitbotWheelSize::kSixInch    // 6" diameter wheels.
+);
+
+frc::AnalogGyro m_gyro{1};
+frc::sim::AnalogGyroSim m_gyroSim{m_gyro};
+
+frc::PWMSparkMax m_leftMotor{0};
+frc::PWMSparkMax m_rightMotor{1};
+
+frc::DifferentialDriveOdometry m_odometry{m_gyro.GetRotation2d(), frc::Pose2d{4_m, 8_m, 0_rad}};
+
+void Robot::RobotInit() {
+	frc::SmartDashboard::PutData("Field", &m_field);
+}
+void Robot::RobotPeriodic() {
+	m_odometry.Update(m_gyro.GetRotation2d(), units::meter_t(m_leftEncoder.GetDistance()), units::meter_t(m_rightEncoder.GetDistance()));
+	m_field.SetRobotPose(m_odometry.GetPose());
+}
+
+
+void Robot::AutonomousInit() {
+	m_leftEncoder.SetDistancePerPulse(wpi::math::pi * 0.1524 / 2048);
+	m_rightEncoder.SetDistancePerPulse(wpi::math::pi * 0.1524 / 2048);
+}
+
 void Robot::AutonomousPeriodic() {
-	_visionValues = _network->dpRecv(_visionValues, true);
-	double xoffset = _visionValues.getDecimals(0) - 320;
-	double rpiDT = _visionValues.getDecimals(2);
+	m_leftMotor.Set(0.2);
+	m_rightMotor.Set(0.2);
 
-	// PID, get dt from rpi and use gyroscope with x coord value
-	double power = pid.pid(rpiDT, _robotMap.driveSystem.gyro.GetAngle(), xoffset);
+	m_driveSim.SetInputs(m_leftMotor.Get() * units::volt_t(frc::RobotController::GetInputVoltage()), m_rightMotor.Get() * units::volt_t(frc::RobotController::GetInputVoltage()));
 
-	_robotMap.driveSystem.drivetrain.Set(power, -power); // power to drive
+	// Advance the model by 20 ms. Note that if you are running this
+	// subsystem in a separate thread or have changed the nominal timestep
+	// of TimedRobot, this value needs to match it.
+	m_driveSim.Update(20_ms);
+
+	// Update all of our sensors.
+	m_leftEncoderSim.SetDistance(m_driveSim.GetLeftPosition().to<double>());
+	m_leftEncoderSim.SetRate(m_driveSim.GetLeftVelocity().to<double>());
+
+	m_rightEncoderSim.SetDistance(m_driveSim.GetRightPosition().to<double>());
+	m_rightEncoderSim.SetRate(m_driveSim.GetRightVelocity().to<double>());
+	m_gyroSim.SetAngle(-m_driveSim.GetHeading().Degrees().to<double>());
 }
 
 void Robot::TeleopInit() {}
