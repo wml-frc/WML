5c4eab227deea152326ec9a53ba61e828a32da11
Connor <connorbuchel@gmail.com>
2021-11-04 20:19:54 +0800

    Fix for .gitmodules and robot

--- Test/src/test/cpp/main.cpp
+++ Test/src/test/cpp/main.cpp
@@ -3,8 +3,8 @@
 #include "gtest/gtest.h"
 
 int main(int argc, char** argv) {
-  HAL_Initialize(500, 0);
-  ::testing::InitGoogleTest(&argc, argv);
-  int ret = RUN_ALL_TESTS();
-  return ret;
+	HAL_Initialize(500, 0);
+	::testing::InitGoogleTest(&argc, argv);
+	int ret = RUN_ALL_TESTS();
+	return ret;
 }
